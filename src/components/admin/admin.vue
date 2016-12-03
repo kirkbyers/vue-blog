e7784260317aea4491ba0f5ed319f0774c59df7b
Kirk byers <kingbyers@gmail.com>
2016-12-03 15:34:32 -0600

    Scafold out admin route.

---
+++ src/components/admin/admin.vue
@@ -0,0 +1,38 @@
+<template>
+    <div id="admin-main">
+        <h1>Admin pannel</h1>
+        <div>
+            <h2>Drafts</h2>
+            <ul>
+                <li v-for="draft in blogDrafts">
+                    <router-link v-bind:to="{ name: 'draftEdit', params: { draftId: draft['.key'] } }">{{draft.title}}</router-link>
+                </li>
+            </ul>
+        </div>
+        <hr>
+        <div>
+            <h2>Posts</h2>
+            <ul>
+                <li v-for="post in blogPosts">
+                    <router-link :to="{name: 'postEdit', params: { postId: post['.key']}}">{{post.title}}</router-link>
+                </li>
+            </ul>
+        </div>
+    </div>
+</template>
+
+<script>
+import firebase from '../../services/firebase';
+
+const db = firebase.database();
+
+export default {
+    name: 'admin-main',
+    firebase: {
+        blogPosts: db.ref('/posts'),
+        blogDrafts: db.ref('/drafts')
+    }
+}
+</script>
+
+<style></style>
--- src/routes.js
+++ src/routes.js
@@ -1,13 +1,14 @@
 import PublicAppComponent from './components/public-app/public-app.vue';
 import BlogPostsComponent from './components/blog-posts/blog-posts.vue';
 import LoginComponent from './components/login/login.vue';
+import AdminComponent from './components/admin/admin.vue';
 
 import App from './App.vue';
 
 const routes = [
     {
         path: '/',
-        name: 'public-app',
+        name: 'home',
         component: PublicAppComponent,
         children: [
             {
@@ -21,6 +22,19 @@ const routes = [
         path: '/login',
         name: 'login',
         component: LoginComponent
+    },
+    {
+        path: '/admin',
+        name: 'admin',
+        component: AdminComponent
+    },
+    {
+        path: '/draftedit/:draftId',
+        name: 'draftEdit'
+    },
+    {
+        path: '/postedit/:postId',
+        name: 'postEdit'
     }
 ];
 
